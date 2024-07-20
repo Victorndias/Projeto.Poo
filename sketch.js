0b5d5c0fb63342f91cf279228e16a57b08212df9
Mateus Victor <mateusvictorct@gmail.com>
2024-07-20 09:48:30 -0300

    add os primeiros codigos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+var y = 5;
+let cursorImg;
+
+function preload(){
+  cursorImg = loadImage("nave.png");
+}
 function setup() {
   createCanvas(400, 400);
+  x = random(50,350);
+  noCursor();
 }
 
 function draw() {
-  background(220);
+  background('blue');
+  
+  rect(x,y,15,50);
+    y++;
+  
+  if(y > 400){
+    x = random(50,350);
+    y= 0;
+  }
+   
+  image(cursorImg,mouseX,mouseY);
 }
