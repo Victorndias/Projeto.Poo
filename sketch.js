abe4002d00737611042f121d664515a917dbb925
Victor Nunes <victor.nuness787@gmail.com>
2024-07-22 20:47:45 -0300

    nova função

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 var y = 5;
 let cursorImg;
+var disparoAtivo = false;
+var xd;
+var yd;
 
 function preload(){
   cursorImg = loadImage("nave.png");
@@ -15,6 +18,22 @@ function draw() {
   
   rect(x,y,15,50);
     y++;
+
+    //toda vez que o for pressionado é ativado//
+    if(mouseIsPressed && ! disparoAtivo){
+      disparoAtivo = true;
+      yd = mouseY;
+      xd = mouseX;
+    }
+
+    //tamanho e velocidade do disparo//
+    if(disparoAtivo){
+      ellipse(xd, yd, 5, 10);
+        yd = yd -8;
+        if(yd < 0){
+          disparoAtivo = false;
+        }
+      }
   
   if(y > 400){
     x = random(50,350);
