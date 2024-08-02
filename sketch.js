7f7e42a74da7229dae96cd64cec4eba1848ed057
Guiguidaa <65306004+Guiguidaa@users.noreply.github.com>
2024-08-02 17:50:42 -0300

    alteração em sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-//Variáveis//
 let cursorImg;
 let AlvoImg;
 let meteoroImg;
@@ -8,7 +7,7 @@ let estrelas = [];
 let vida = 3;
 let pontuacao = 0;
 
-//Função para carregar a imegem e deixar pronto pra uso//
+//Função para carregar a imegem e deixar pronto pra uso
 function preload() {
   meteoroImg = loadImage("asteroid.gif");
   cursorImg = loadImage("nave.png");
@@ -27,7 +26,6 @@ function setup() {
 
   disparo = new Disparo();
 
-  //for responsável por criar um lop e adiciona estrelas até chegar na quantidade desejada//
   for (let i = 0; i < 5; i++) {
     estrelas.push(new Estrela());
   }
@@ -50,7 +48,7 @@ function draw() {
   if (disparo.ativo) {
     disparo.move();
     disparo.display();
-//Para verificar se a nave inimiga foi destruida e pontua//
+//Para verificar se a nave inimiga foi destruída e pontuar
     if (disparo.acertou(nave)) {
       disparo.ativo = false;
       pontuacao++;
@@ -58,55 +56,53 @@ function draw() {
     }
   }
 
-for (let estrela of estrelas) {
+  for (let estrela of estrelas) {
     estrela.display();
     estrela.update();
-}
+  }
 
-  //Verifica se o alvo saiu da tela ou não//
-    meteoro.verificaSaida();
-    meteoro2.verificaSaida();
-    meteoro3.verificaSaida();
-    meteoro4.verificaSaida();
-    nave.verificaSaida();
-  //O objetivo mostra algumas coisa na interface//
+//Verifica se o alvo saiu da tela ou não
+  meteoro.verificaSaida();
+  meteoro2.verificaSaida();
+  meteoro3.verificaSaida();
+  meteoro4.verificaSaida();
+  nave.verificaSaida();
+//O objetivo mostra algumaas coisas na interface
   displayHUD();
 
-  //IF para observar se a vida ficou menor ou igual a 0//
+//IF para observar se a vida ficou menor ou igual a 0
   if (vida <= 0) {
     gameOver();
   }
 }
 
-//função para verificar se o mouse foi pressionado, se sim a ação é efetuado//
 function mousePressed() {
-  if (!disparo.ativo) {
+    if (!disparo.ativo) {
     disparo.ativar(mouseX, mouseY);
   }
 }
 
-//Classe Alvo//
+//Classe Alvo
 class Nave {
-  
+
   constructor() {
-//Gerar uma possicão aleatória no eixo x//
     this.xNave = random(50, 850);
     this.y = 5;
   }
-//parte responsável pelo movimento da nave//
-    move()  {
+//parte responsável pelo movimento da nave
+    move() {
     this.y++;
   }
     display() {
     image(AlvoImg, this.xNave, this.y);
   }
     reset() {
-//Gerar uma possicão aleatória no eixo x//
+//gera uma posição aleatória no eixo x
     this.xNave = random(50, 850);
     this.y = Math.random(0, 850);
   }
+//Verifica se o Objetivo saiu das dimensões e diminui -1 na vida
   verificaSaida() {
-//Verifica se o objeto saiu das dimessão e diminui -1 na vida//
     if (this.y > height) {
       this.reset();
       vida--;
@@ -114,11 +110,12 @@ class Nave {
   }
 }
 
-//Classe Disparo//
+//Classe meteoro
 class Meteoro {
+
   constructor() {
-    this.xMeteoro = random(40, 850);
-   this.y = random(0, 150);
+  this.xMeteoro = random(40, 850);
+  this.y = random(0, 150);;
   }
   move() {
     this.y++;
@@ -127,46 +124,47 @@ class Meteoro {
     image(meteoroImg, this.xMeteoro, this.y);
   }
   reset() {
-    this.xMeteoro = random(40, 850);
-   this.y = random(0, 150);;
+  this.xMeteoro = random(40, 850);
+  this.y = random(0, 150);;
   }
   verificaSaida() {
     if (this.y > height) {
-      this.reset();
+        this.reset();
     }
   }
 }
+
 class Disparo {
   constructor() {
     this.x = 0;
     this.y = 0;
     this.ativo = true;
   }
+
   ativar(x, y) {
     this.x = x;
     this.y = y;
     this.ativo = true;
   }
-//velocidade do disparo//
+  //Velocidade do disparo
   move() {
     this.y -= 8;
     if (this.y < 0) {
       this.ativo = false;
     }
   }
-
-//Cor, tamanho e deixar centralizado o disparo da nave//
+//Cor, tamanho e centralização do disparo da nave
   display() {
     fill('yellow');
     ellipse(this.x+ cursorImg.width /2, this.y, 15, 10);
   }
-//Trecho responsável por verificar se acertou ou o alvo// 
+//trecho responsável por verificar o acerto do alvo
   acertou(alvo) {
     return this.x > alvo.xNave && this.x < alvo.xNave + AlvoImg.width &&
            this.y > alvo.y && this.y < alvo.y + AlvoImg.height;
   }
 }
-//Classe Estrela//
+//Classe Estrela
 class Estrela {
   constructor() {
     this.x = random(width);
@@ -185,24 +183,25 @@ class Estrela {
     pop();
   }
 }
+
 function displayHUD() {
-// Desenha a imagem do cursor//
+// Desenha a imagem do cursor
   image(cursorImg, mouseX, mouseY);
 
-  // Desenha a barra de vida//
+// Desenha a barra de vida
   fill('red');
   rect(10, 10, vida * 30, 20);
   noFill();
   stroke('white');
-  // Moldura da barra de vida//
-  rect(10, 10, 90, 20);
-  // Desenha a pontuação//
+// Moldura da barra de vida
+  rect(10, 10, 90, 20); 
+
+// Desenha a pontuação
   fill('white');
   textSize(15);
-  text("Pontuação: " + pontuacao, 10, 50);
+  text("Pontuação:" + pontuacao, 10, 50);
 }
 
-// Desenha Game Over na tela//
 function gameOver() {
   noLoop();
   fill('red');
