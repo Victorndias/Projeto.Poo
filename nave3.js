ae66458f25d264029f0e4f3ed0babc7ce9ff4fef
Mateus Victor <mateusvictorct@gmail.com>
2024-09-08 09:54:49 -0300

    alteração na moviementação da nave para os lados

--- nave3.js
+++ nave3.js
@@ -1,10 +1,24 @@
 class Nave3 extends Nave {
-    constructor(x, y) {
-      super(x, y);
-    }
-
-    display() {
-      image(Inimigo2Img, this.xNave, this.y);
-    }
+  constructor(x, y) {
+    super(x, y);
+    this.velocidade = 1;
+    this.direcao = 1;
 
   }
+
+  display() {
+    image(Inimigo2Img, this.xNave, this.y);
+  }
+  move() {
+    if (this.xNave < width && this.direcao === 1) {
+      this.xNave += this.velocidade;
+    } else if (this.xNave >= width) {
+      this.direcao = -1;
+    } else if (this.xNave <= 0) {
+      this.direcao = 1;
+    }
+    this.xNave += this.direcao;
+    this.y += this.velocidade;
+
+  }
+}
